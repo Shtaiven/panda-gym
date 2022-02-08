f8d6daf58aa8d22eeed347f6404476150c6c7d9e
Steven Eisinger <steven.a.eisinger@gmail.com>
2022-02-08 14:39:33 +0100

    Added PandaReach test script

---
+++ test/panda_reach_test.py
@@ -0,0 +1,18 @@
+import time
+import gym
+import panda_gym
+
+
+def main():
+    env = gym.make('PandaReach-v2', render=True, control_type='joints', obstacle_type="bin", reward_type="dense")
+
+    for _ in range(10):
+        obs, done = env.reset(), False
+
+        while not done:
+            obs, reward, done, info = env.step(env.action_space.sample())
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
